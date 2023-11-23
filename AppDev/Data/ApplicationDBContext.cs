2ba9457c032dff781b53f44fd4d7904fdfc9e1b4
84796 <84796@KIMTHAORRRR>
2023-11-23 10:04:13 +0700

    Add project files.

---
+++ AppDev/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace AppDev.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
